a55d77d89b54daff0857c54226387392a1dae6ea
Jorge <jltamayocabello@gmail.com>
2026-07-19 21:24:00 -0400

    Se comenta código

--- ARCHIVOS/AccesoDatos.js
+++ ARCHIVOS/AccesoDatos.js
@@ -1,3 +1,5 @@
+// Acceso a datos
+
 let persona =
 {
     "nombre": "John Doe",
@@ -7,10 +9,13 @@ let persona =
     "colores": ["rojo", "verde", "azul"]
 }
 
+// Acceder a los datos
 console.log(persona);
 
+// Acceder a los datos desde la variable
 let nombre = persona.nombre;
 console.log("El nombre es: ", nombre);
 
+// Acceder a los datos de un array
 let color = persona.colores[2];
 console.log("El color es: ", color);
