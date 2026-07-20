e926eea8fa40a097d5ec1cc6baba14cbd3574efb
Jorge <jltamayocabello@gmail.com>
2026-07-19 21:22:03 -0400

    Ejemplo de como acceder a un elemento de un array

--- ARCHIVOS/AccesoDatos.js
+++ ARCHIVOS/AccesoDatos.js
@@ -3,10 +3,14 @@ let persona =
     "nombre": "John Doe",
     "edad": 30,
     "ciudad": "Lima",
-    "pais": "Peru"
+    "pais": "Peru",
+    "colores": ["rojo", "verde", "azul"]
 }
 
 console.log(persona);
 
 let nombre = persona.nombre;
 console.log("El nombre es: ", nombre);
+
+let color = persona.colores[2];
+console.log("El color es: ", color);
