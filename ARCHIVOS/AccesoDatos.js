715799d364201bd784cb0db98c2a1eb108c7b9e5
Jorge <jltamayocabello@gmail.com>
2026-07-19 21:26:59 -0400

    Se añade ejemplo de acceso a datos de un array de objetos

--- ARCHIVOS/AccesoDatos.js
+++ ARCHIVOS/AccesoDatos.js
@@ -6,7 +6,19 @@ let persona =
     "edad": 30,
     "ciudad": "Lima",
     "pais": "Peru",
-    "colores": ["rojo", "verde", "azul"]
+    // Array
+    "colores": ["rojo", "verde", "azul"],
+    // Array de objetos
+    "equipos_electronicos": [
+        {
+            "nombre": "Radio",
+            "peso": "1 kg"
+        },
+        {
+            "nombre": "Televisor",
+            "peso": "2 kg"
+        }
+    ]
 }
 
 // Acceder a los datos
@@ -19,3 +31,7 @@ console.log("El nombre es: ", nombre);
 // Acceder a los datos de un array
 let color = persona.colores[2];
 console.log("El color es: ", color);
+
+// Acceder a los datos de un array de objetos
+let radio = persona.equipos_electronicos[0];
+console.log("El radio pesa: ", radio.peso);
